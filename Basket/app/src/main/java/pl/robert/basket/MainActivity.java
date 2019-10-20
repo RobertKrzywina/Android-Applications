7e6a424a7e8fcbf9fc0b2ee280f22635418c121b
RobertKrzywina <robert.krzywina@gmail.com>
2019-10-20 21:26:26 +0200

    Basket: Replace square by fruits

--- Basket/app/src/main/java/pl/robert/basket/MainActivity.java
+++ Basket/app/src/main/java/pl/robert/basket/MainActivity.java
@@ -11,6 +11,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -27,8 +29,8 @@ public class MainActivity extends AppCompatActivity {
     static boolean hasUserTapped = false;
     private boolean hasUserStarted = false;
 
-    private MovableElement basket;
-    private MovableElement square;
+    private Basket basket;
+    private List<Fruit> fruits;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,7 +49,8 @@ public class MainActivity extends AppCompatActivity {
             checkIfUserTapped(event);
         } else {
             this.hasUserStarted = true;
-            initializeMovableElements();
+            configureBasket();
+            configureFruits();
             setScreenHeight();
             setFrameWidth();
             setVisibilityOfTextViews();
@@ -64,9 +67,42 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void initializeMovableElements() {
-        this.basket = new Basket((ImageView) findViewById(R.id.basket));
-        this.square = new Fruit((ImageView) findViewById(R.id.square));
+    private void configureBasket() {
+        ImageView basketImg = findViewById(R.id.basket);
+        basket = new Basket(basketImg);
+        basketImg.setVisibility(View.VISIBLE);
+    }
+
+    private void configureFruits() {
+        ImageView fruit1Img = findViewById(R.id.fruit1);
+        ImageView fruit2Img = findViewById(R.id.fruit2);
+        ImageView fruit3Img = findViewById(R.id.fruit3);
+        ImageView fruit4Img = findViewById(R.id.fruit4);
+        ImageView fruit5Img = findViewById(R.id.fruit5);
+        ImageView fruit6Img = findViewById(R.id.fruit6);
+        ImageView fruit7Img = findViewById(R.id.fruit7);
+        ImageView fruit8Img = findViewById(R.id.fruit8);
+        ImageView fruit9Img = findViewById(R.id.fruit9);
+
+        setVisibilityForFruitsImages(
+                Arrays.asList(
+                        fruit1Img, fruit2Img, fruit3Img,
+                        fruit4Img, fruit5Img, fruit6Img,
+                        fruit7Img, fruit8Img, fruit9Img
+                )
+        );
+
+        fruits = Arrays.asList(
+                new Fruit(fruit1Img), new Fruit(fruit2Img), new Fruit(fruit3Img),
+                new Fruit(fruit4Img), new Fruit(fruit5Img), new Fruit(fruit6Img),
+                new Fruit(fruit7Img), new Fruit(fruit8Img), new Fruit(fruit9Img)
+        );
+    }
+
+    private void setVisibilityForFruitsImages(List<ImageView> fruitsImages) {
+        for (ImageView fruitImg : fruitsImages) {
+            fruitImg.setVisibility(View.VISIBLE);
+        }
     }
 
     private void setScreenHeight() {
@@ -95,8 +131,11 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void run() {
                         basket.animate(20);
-                        square.animate(12);
-                        checkHit();
+                        for (MovableElement fruit : fruits) {
+                            int randomSpeed = (int) ((Math.random() * 10) + 10);
+                            fruit.animate(randomSpeed);
+                            checkHit(fruit);
+                        }
                     }
                 });
             }
@@ -104,16 +143,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * Check if element has fallen into basket.
+     * Check if fruit has fallen into basket.
      * If yes then increase current score and move element outside of screen.
+     *
+     * @param fruit to check
      */
-    private void checkHit() {
-        int difference = square.getX() - basket.getX();
+    private void checkHit(MovableElement fruit) {
+        int difference = fruit.getX() - basket.getX();
         boolean hasElementFallenIntoBasket =
-                square.getY() >= basket.getY() && difference > 0 && difference <= basket.getWidth();
+                fruit.getY() >= basket.getY() && difference > 0 && difference <= basket.getWidth();
         if (hasElementFallenIntoBasket) {
             score.setText(String.valueOf(++currentScore));
-            square.moveToBottom(screenHeight + 1);
+            fruit.moveToBottom(screenHeight + 1);
         }
     }
 }
