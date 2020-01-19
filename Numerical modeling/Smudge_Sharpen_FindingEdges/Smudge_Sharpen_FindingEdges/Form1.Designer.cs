1197891529adcc90c499ce1ea28579748afd5046
RobertKrzywina <robert.krzywina@gmail.com>
2020-01-19 14:04:42 +0100

    Add Smudge_Sharpen_FindingEdges app

---
+++ Numerical modeling/Smudge_Sharpen_FindingEdges/Smudge_Sharpen_FindingEdges/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Smudge_Sharpen_FindingEdges
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button = new System.Windows.Forms.Button();
+            this.smudgeBox = new System.Windows.Forms.PictureBox();
+            this.sharpenBox = new System.Windows.Forms.PictureBox();
+            this.findingEdgesBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.smudgeBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sharpenBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.findingEdgesBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button
+            // 
+            this.button.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button.Location = new System.Drawing.Point(242, 34);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(224, 40);
+            this.button.TabIndex = 0;
+            this.button.Text = "Load";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click_1);
+            // 
+            // smudgeBox
+            // 
+            this.smudgeBox.Location = new System.Drawing.Point(12, 127);
+            this.smudgeBox.Name = "smudgeBox";
+            this.smudgeBox.Size = new System.Drawing.Size(224, 149);
+            this.smudgeBox.TabIndex = 1;
+            this.smudgeBox.TabStop = false;
+            // 
+            // sharpenBox
+            // 
+            this.sharpenBox.Location = new System.Drawing.Point(242, 127);
+            this.sharpenBox.Name = "sharpenBox";
+            this.sharpenBox.Size = new System.Drawing.Size(224, 149);
+            this.sharpenBox.TabIndex = 2;
+            this.sharpenBox.TabStop = false;
+            // 
+            // findingEdgesBox
+            // 
+            this.findingEdgesBox.Location = new System.Drawing.Point(472, 127);
+            this.findingEdgesBox.Name = "findingEdgesBox";
+            this.findingEdgesBox.Size = new System.Drawing.Size(224, 149);
+            this.findingEdgesBox.TabIndex = 3;
+            this.findingEdgesBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(714, 288);
+            this.Controls.Add(this.findingEdgesBox);
+            this.Controls.Add(this.sharpenBox);
+            this.Controls.Add(this.smudgeBox);
+            this.Controls.Add(this.button);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.smudgeBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sharpenBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.findingEdgesBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.PictureBox smudgeBox;
+        private System.Windows.Forms.PictureBox sharpenBox;
+        private System.Windows.Forms.PictureBox findingEdgesBox;
+    }
+}
+
---
+++ Numerical modeling/Smudge_Sharpen_FindingEdges/Smudge_Sharpen_FindingEdges/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Smudge_Sharpen_FindingEdges
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap mainMap;
+        private Bitmap bm1;
+        private Bitmap bm2;
+        private Bitmap bm3;
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetCenterScreen();
+        }
+
+        private void SetCenterScreen()
+        {
+            StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void button_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "Image Files(*.jpg; *.jpeg; *.png; *.gif; *.bmp)|*.jpg; *.jpeg; *.png; *.gif; *.bmp";
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                mainMap = new Bitmap(file.FileName);
+                bm1 = new Bitmap(Image.FromFile(file.FileName));
+                bm2 = new Bitmap(Image.FromFile(file.FileName));
+                bm3 = new Bitmap(Image.FromFile(file.FileName));
+
+                int width = mainMap.Width;
+                int height = mainMap.Height;
+                for (int i=1; i<height-1; i++)
+                {
+                    for (int j=1; j<width-1; j++)
+                    {
+                        Color c1 = mainMap.GetPixel(j, i);
+                        Color c2 = mainMap.GetPixel(j - 1, i);
+                        Color c3 = mainMap.GetPixel(j + 1, i);
+                        Color c4 = mainMap.GetPixel(j - 1, i - 1);
+                        Color c5 = mainMap.GetPixel(j - 1, i + 1);
+                        Color c6 = mainMap.GetPixel(j, i + 1);
+                        Color c7 = mainMap.GetPixel(j, i - 1);
+                        Color c8 = mainMap.GetPixel(j + 1, i + 1);
+                        Color c9 = mainMap.GetPixel(j + 1, i - 1);
+                        int sR = ((c1.R + c2.R + c3.R + c4.R + c5.R + c6.R + c7.R + c8.R + c9.R) / 9);
+                        int sG = ((c1.G + c2.G + c3.G + c4.G + c5.G + c6.G + c7.G + c8.G + c9.G) / 9);
+                        int sB = ((c1.B + c2.B + c3.B + c4.B + c5.B + c6.B + c7.B + c8.B + c9.B) / 9);
+
+                        c1 = mainMap.GetPixel(j, i - 1);
+                        c2 = mainMap.GetPixel(j - 1, i);
+                        c3 = mainMap.GetPixel(j, i);
+                        c4 = mainMap.GetPixel(j + 1, i);
+                        c5 = mainMap.GetPixel(j, i + 1);
+                        int r = c1.R + c2.R + c3.R * (-4) + c4.R + c5.R;
+                        int g = c1.G + c2.G + c3.G * (-4) + c4.G + c5.G;
+                        int b = c1.B + c2.B + c3.B * (-4) + c4.B + c5.B;
+
+                        int avg = (r + g + b) / 3;
+                        if (avg > 255) avg = 255;
+                        if (avg < 0) avg = 0;
+
+                        bm1.SetPixel(j, i, Color.FromArgb(avg, avg, avg));
+                        bm3.SetPixel(j, i, Color.FromArgb(sR, sG, sB));
+
+                        smudgeBox.Image = bm3;
+                        sharpenBox.Image = bm1;
+                        findingEdgesBox.Image = bm2;
+                    }
+                }
+            }
+        }
+    }
+}
