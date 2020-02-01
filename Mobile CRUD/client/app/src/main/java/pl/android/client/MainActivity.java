9255544a65c55323aa8591680d6cfdee0b550d6f
RobertKrzywina <robert.krzywina@gmail.com>
2020-02-01 16:45:00 +0100

    Add btns and textView for UI

---
+++ Mobile CRUD/client/app/src/main/java/pl/android/client/MainActivity.java
@@ -0,0 +1,38 @@
+package pl.android.client;
+
+import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void add(View view) {
+
+    }
+
+    public void getAll(View view) {
+
+    }
+
+    public void getByEmail(View view) {
+
+    }
+
+    public void updateAge(View view) {
+
+    }
+
+    public void deleteByEmail(View view) {
+
+    }
+
+    public void deleteAll(View view) {
+
+    }
+}
